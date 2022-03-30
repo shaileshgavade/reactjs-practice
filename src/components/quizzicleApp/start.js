4c50367cc4f8601a60efb96980147d137b72309f
shailesh <shailesh.gavade@intuio.io>
2022-03-30 18:11:36 +0530

    [TASK] change quiz desciption

--- src/components/quizzicleApp/start.js
+++ src/components/quizzicleApp/start.js
@@ -4,7 +4,7 @@ export default function Start(props) {
     return (
         <div className="quiz--start">
             <h3>Quizzicle</h3>
-            <p>quizzicle app game</p>
+            <p>quizzicle game app</p>
             <button className="quiz--start-button" onClick={props.startNewQuiz}>Start</button>
         </div>
     )
